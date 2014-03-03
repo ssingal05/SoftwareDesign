5baccd78eb1cc949f681ed69107de8ff4c3fc426
Siddharth Singal <ssingal05@gmail.com>
2014-03-03 14:58:17 -0500

    added files

--- hw5/Wikipedia_race_engine.py
+++
@@ -1,8 +0,0 @@
-#from pattern.web import Facebook
-#f = Facebook(license='CAAEuAis8fUgBAIl50YTbdkqZBNhZCp7Go335k3BHYZCcx40yWmXIMVhFZBnCzislDJZAzohMnLZAznndgVZB1e6aC6Kx8hRBKpbvXvC5VxTrXNsskXycXRvEdaoa9WhOiMSzxTxfKicppi1JKoWlyZAfp2v2rS74pComRBKnbvE0c7gG81On0oEr')
-#me = f.profile()
-#print f.search('hello')
-#print me
-
-from pattern.web import Wikipedia
-w = Wikipedia()
